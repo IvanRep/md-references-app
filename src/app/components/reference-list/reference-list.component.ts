efe2d193ebfc10e91c8b5c4c230124e90e768579
IvanRep <ivanparra.develop@gmail.com>
2024-03-20 11:33:09 +0100

    feat: Add referecnce list

---
+++ src/app/components/reference-list/reference-list.component.ts
@@ -0,0 +1,50 @@
+import { Component, Input } from '@angular/core';
+import { type Reference } from '@/app/types/Reference';
+import { ReferenceComponent } from '@/app/components/reference/reference.component';
+
+@Component({
+  selector: 'app-reference-list',
+  standalone: true,
+  imports: [ReferenceComponent],
+  templateUrl: './reference-list.component.html',
+  styleUrl: './reference-list.component.css',
+})
+export class ReferenceListComponent {
+  @Input() referencesList: Reference[] = [
+    {
+      name: 'Deportes Mazarracin',
+      place: 'PI. Esparto, 5, 28341 Valdemoro, Madrid, España',
+      category: 'Churros de piscina',
+      phone: '912 48 08 73',
+      website: 'https://www.deportesmazarracin.com/',
+    },
+    {
+      name: 'Deportes Mazarracin',
+      place: 'PI. Esparto, 5, 28341 Valdemoro, Madrid, España',
+      category: 'Churros de piscina',
+      phone: '912 48 08 73',
+      website: 'https://www.deportesmazarracin.com/',
+    },
+    {
+      name: 'Deportes Mazarracin',
+      place: 'PI. Esparto, 5, 28341 Valdemoro, Madrid, España',
+      category: 'Churros de piscina',
+      phone: '912 48 08 73',
+      website: 'https://www.deportesmazarracin.com/',
+    },
+    {
+      name: 'Deportes Mazarracin',
+      place: 'PI. Esparto, 5, 28341 Valdemoro, Madrid, España',
+      category: 'Churros de piscina',
+      phone: '912 48 08 73',
+      website: 'https://www.deportesmazarracin.com/',
+    },
+    {
+      name: 'Deportes Mazarracin',
+      place: 'PI. Esparto, 5, 28341 Valdemoro, Madrid, España',
+      category: 'Churros de piscina',
+      phone: '912 48 08 73',
+      website: 'https://www.deportesmazarracin.com/',
+    },
+  ];
+}
--- src/app/pages/references/references.component.ts
+++ src/app/pages/references/references.component.ts
@@ -1,12 +1,12 @@
 import { HeaderComponent } from '@/app/components/header/header.component';
-import { ReferenceComponent } from '@/app/components/reference/reference.component';
+import { ReferenceListComponent } from '@/app/components/reference-list/reference-list.component';
 import { UserLoginComponent } from '@/app/components/user-login/user-login.component';
 import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-references',
   standalone: true,
-  imports: [HeaderComponent, UserLoginComponent, ReferenceComponent],
+  imports: [HeaderComponent, UserLoginComponent, ReferenceListComponent],
   templateUrl: './references.component.html',
   styleUrl: './references.component.css',
 })
